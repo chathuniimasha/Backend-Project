8d3076fd4c1230868ef1dcfc992d707b21b632f2
chathuniimasha <chathuniimasha200267@gmail.com>
2025-11-12 18:03:19 +0530

    👍

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,17 @@ dotenv.config()
 
 const transporter = nodemailer.createTransport({
 	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
+	port: 587,
+	secure: false,
 	auth: {
 		user: process.env.EMAIL_USER,
 		pass: process.env.EMAIL_PASS,
 	},
+    connectionTimeout: 15000,
+    greetingTimeout: 10000,
+    socketTimeout: 15000,
+    debug: true, // Remove in production
+    logger: true // Remove in production
 });
 
 export function createUser(req,res){
